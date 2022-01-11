1295081cc05c2ac2008d35b519c30e1ff9ac3cc6
Yoko <zartem01@gmail.com>
2022-01-11 17:35:57 +0300

    mini tests for db

--- format_data/tests/db_tests.py
+++ format_data/tests/db_tests.py
@@ -1,11 +1,17 @@
-import unittest, sys
+import unittest, sys, json
 sys.path.append('../')
 from utils.db import Database
 
 class TestDBMethods(unittest.TestCase):
 
-     def setUp(self):
-         self.db = Database()
+    def setUp(self):
+        self.db = Database()
+
+    def test_insert(self):
+        data_json = {"config": "test"}
+        json_object = json.dumps(data_json)
+        self.assertTrue(self.db.insert('format_data_test', 4, 1, 'test_name', 
+            'desc', json_object, 'test_type', 'test_infra_type', 2, 3))
 
 if __name__ == '__main__':
     unittest.main()
--- format_data/utils/func.py
+++ format_data/utils/func.py
@@ -2,9 +2,12 @@ import configparser, os, sys
 
 def get_config():
     config = configparser.ConfigParser()
+    path = 'src'
+    if not os.path.exists(path):
+        path = '../' + path
 
-    for filename in os.listdir(f'../src'):
+    for filename in os.listdir(path):
         if filename.endswith('.ini'):
-            config.read(f'../src/{filename}', encoding = 'UTF8')
+            config.read(f'{path}/{filename}', encoding = 'UTF8')
 
     return config
